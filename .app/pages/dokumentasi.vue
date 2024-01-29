f6e78b725795ac2ff43dbd30d1c827bb970c3242
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2024-01-29 08:23:23 +0700

    sidebar config

---
+++ .app/pages/dokumentasi.vue
@@ -0,0 +1,13 @@
+<script setup>
+definePageMeta(
+{
+  title: 'Dokumentasi',
+  description:
+    'Jelajahi Fitur fitur dan halaman halaman kami.',
+  layout: 'sidebar',
+})
+</script>
+
+<template>
+ tes
+</template>
