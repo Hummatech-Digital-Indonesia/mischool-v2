22e1588c90f2bb37f77584a8e54378f5317c8f99
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2024-02-12 08:10:04 +0700

    mischool

---
+++ .app/pages/dokumentasi/sekolah/kop-surat.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  title: 'Kop Surat',
+  description: 'Jelajahi Fitur fitur dan halaman halaman kami.',
+  layout: 'sidebar',
+})
+</script>
+
+<template>
+  tes
+</template>
