63a1167a01237bb1d99703742b7f926c6b4bd965
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2024-03-13 09:44:59 +0700

    fix bug logo

--- layers/tairo/components/global/TairoLogoText.vue
+++ layers/tairo/components/global/TairoLogoText.vue
@@ -1,42 +1,4 @@
-<script setup>
-const colorMode = useColorMode()
-
-const colorPreference = computed(() => {
-  return colorMode.preference
-})
-</script>
-
 <template>
-  <svg
-    viewBox="0 0 2303 611"
-    fill="none"
-    class="shrink-0"
-    xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
-  >
-    <rect width="2303" height="611" fill="url(#pattern0)" />
-    <defs>
-      <pattern
-        id="pattern0"
-        patternContentUnits="objectBoundingBox"
-        width="1"
-        height="1"
-      >
-        <use
-          xlink:href="#image0_5516_5474"
-          transform="matrix(0.000708215 0 0 0.00266943 0 -0.000517542)"
-        />
-      </pattern>
-      <image
-        id="image0_5516_5474"
-        width="1412"
-        height="375"
-        :xlink:href="
-          colorPreference == 'light'
-            ? '/img/logos/logo-text.png'
-            : '/img/logos/logo-text-dark.png'
-        "
-      />
-    </defs>
-  </svg>
+  <img src="/img/logos/logo-text.png" width="120" alt="logo-mischool" class="block dark:hidden">
+  <img src="/img/logos/logo-text-dark.png" width="120" alt="logo-mischool" class="hidden dark:block">
 </template>
