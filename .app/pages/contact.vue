8cab8c0a596d9ba55778985f170ec72c651b313d
frendikawa <frsnaifla@gmail.com>
2024-05-28 15:18:24 +0700

    contact

--- .app/pages/contact.vue
+++ .app/pages/contact.vue
@@ -9,43 +9,51 @@ definePageMeta({
 
 <template>
   <div class="dark:bg-muted-900 bg-white pt-16">
-    <div class="my-10">
-      <div class="flex flex-col items-center justify-center">
-        <BaseCard rounded="md" shadow="flat" class="p-6 max-w-2xl">
-          <BaseHeading as="h1" size="lg" weight="semibold"
-            class="flex items-center justify-center text-muted-800 mb-2 dark:text-white">
-            Hubungi Kami
-          </BaseHeading>
-          <BaseInput rounded="md" label="Name" placeholder="Nama.." weight="semibold" />
-          <BaseInput rounded="md" label="Email" placeholder="pengguna@email.com" />
-          <BaseTextarea label="Pesan" rounded="md" placeholder="Tuliskan pesan.." size="md" autogrow />
-          <BaseButton variant="solid" color="primary" class="w-full my-3">
-            Kirim
-          </BaseButton>
-        </BaseCard>
-
-        <div class="flex gap-3 my-3 max-w-2xl">
-              <BaseCard rounded="md" class="p-6 flex justify-center items-center max-w-xl" shadow="flat">
-                <BaseParagraph size="md" weight="semibold">
-                  <Icon name="ph:whatsapp-logo" />
-                  0851-7677-7785
-                </BaseParagraph>
-              </BaseCard>
-              <BaseCard rounded="md" class="p-6 flex justify-center items-center max-w-xl" shadow="flat">
-                <BaseParagraph size="md" weight="semibold">
-                  <Icon name="ph:envelope" />
-                  info@hummatech.com
-                </BaseParagraph>
-              </BaseCard>
+    <div class="my-10 grid flex-col justify-center items-center">
+      <BaseCard rounded="md" shadow="flat" class="p-6 max-w-2xl">
+        <BaseHeading as="h1" size="md" weight="semibold"
+          class="text-muted-800 mb-2 dark:text-white flex justify-center items-center">
+          Hubungi Kami
+        </BaseHeading>
+        <BaseInput rounded="md" label="Nama" placeholder="Nama Pengguna.." />
+        <BaseInput rounded="md" label="Email" placeholder="example@email.com" />
+        <BaseTextarea contrast="default-contrast" size="md" label="Pesan" rounded="md" placeholder="Tuliskan Pesan"
+          color-focus />
+        <BaseButton variant="solid" color="primary" class="w-full mt-2">
+          Kirim
+        </BaseButton>
+      </BaseCard>
+      <div class="grid grid-cols-12 max-w-2xl justify-center gap-2 my-2">
+        <div class="col-span-6">
+          <a href="https://wa.me/6282132560566">
+            <BaseCard rounded="md" shadow="flat" class="p-6 flex justify-center w-full gap-2 items-center">
+              <Icon name="ph:whatsapp-logo" class="h-7 w-7" />
+              <BaseParagraph size="md" weight="semibold">
+                (+62)82132560566
+              </BaseParagraph>
+            </BaseCard>
+          </a>
         </div>
-        <BaseCard rounded="md" class="p-6 max-w-2xl flex justify-center" shadow="flat">
-          <BaseParagraph size="md" weight="semibold">
-            <Icon name="ph:map-pin" />
-            Perum Permata Regency 1 Blok 10 No. 28, Ngijo, Kec. Karangploso, Kab. Malang, 65152
-          </BaseParagraph>
-        </BaseCard>
+        <div class="col-span-6">
+          <a href="mailto:mischool@hummatech.com">
+            <BaseCard rounded="md" shadow="flat" class="p-6 flex justify-center w-full gap-2 items-center">
+              <Icon name="ph:envelope" class="h-7 w-7" />
+              <BaseParagraph size="md" weight="semibold">
+                mischool@hummatech.com
+                <!-- Perum Permata Regency 1 Blok 10 No. 28, Ngijo, Kec. Karangploso, Kab. Malang, 65152 -->
+              </BaseParagraph>
+            </BaseCard>
+          </a>
+        </div>
       </div>
+      <BaseCard rounded="md" class="p-6 max-w-2xl flex justify-center items-center gap-2" shadow="flat">
+        <Icon name="ph:map-pin" class="h-7 w-7" />
+        <BaseParagraph size="md" weight="semibold">
+          Perum Permata Regency 1 Blok 10 No. 28, Ngijo, Kec. Karangploso, Kab. Malang, 65152
+        </BaseParagraph>
+      </BaseCard>
     </div>
+
     <LandingFooter />
   </div>
 </template>
